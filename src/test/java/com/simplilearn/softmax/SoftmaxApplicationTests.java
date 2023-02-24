33c1338b156823bcfa1fa98a57b051518b81aae4
Rob Michael <robmichael93@gmail.com>
2023-02-24 16:18:06 -0600

    Initial commit

---
+++ src/test/java/com/simplilearn/softmax/SoftmaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.softmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftmaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
